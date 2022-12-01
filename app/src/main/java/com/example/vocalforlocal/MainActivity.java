f3f64f002e904a306aa4bc2a604a3296023cce86
yogesh9245 <yogeshmanghrani.9245@gmail.com>
2022-12-01 18:35:15 +0530

    Initial Welcome page

--- app/src/main/java/com/example/vocalforlocal/MainActivity.java
+++ app/src/main/java/com/example/vocalforlocal/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.vocalforlocal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        View welcomescreen = findViewById(R.id.welcomescreen);
+        View root = welcomescreen.getRootView();
+        root.setBackgroundColor(Color.parseColor("#7e63b8"));
     }
 }
