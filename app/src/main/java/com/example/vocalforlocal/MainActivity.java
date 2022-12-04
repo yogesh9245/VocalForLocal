ab03196c37e0433666b539fc2fa437b994c4bb94
yogesh9245 <yogeshmanghrani.9245@gmail.com>
2022-12-04 14:36:11 +0530

    About page linking and options menu in main activity

--- app/src/main/java/com/example/vocalforlocal/MainActivity.java
+++ app/src/main/java/com/example/vocalforlocal/MainActivity.java
@@ -5,8 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,4 +31,32 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Toast.makeText(this, "Selected Item: " +item.getTitle(), Toast.LENGTH_SHORT).show();
+        switch (item.getItemId()) {
+            case R.id.aboutItem:
+                // do your code
+                Toast.makeText(MainActivity.this,"Presses 1",Toast.LENGTH_SHORT).show();
+                Intent about = new Intent(MainActivity.this,AboutActivity.class);
+                startActivity(about);
+                return true;
+            case R.id.ContactItem:
+
+                // do your code
+                return true;
+            case R.id.homeItem:
+                // do your code
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 }
